6a159402dbd843be0f52fb5f579d606785fb101e
Ryan Davis <rd11490@gmail.com>
2025-10-14 19:48:55 -0400

    Add season 19 patch

--- src/app/win-rate-selectors/models/patch-dates.ts
+++ src/app/win-rate-selectors/models/patch-dates.ts
@@ -7,5 +7,9 @@ export const PATCH_DATES: PatchInfo[] = [
     {
         date: '2025-09-16',
         title: 'Season 18 Mid Season Update'
+    },
+    {
+        date: '2025-10-14',
+        title: 'Season 19'
     }
 ];
